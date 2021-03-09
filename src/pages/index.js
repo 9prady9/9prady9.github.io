0fbcc65a4062ba5f9ff367a47d9b714c2cbab8b5
Pradeep Garigipati <pradeep.garigipati@gmail.com>
2021-03-09 14:29:53 +0530

    Update packages and add Intel course instructor badge

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,10 +14,14 @@ export default function Home({data}) {
         name="AboutPradeep"
         content="General tech info on Pradeep Garigipati"
       />
+      <script
+        type="text/javascript"
+        src="http://cdn.youracclaim.com/assets/utilities/embed.js" async>
+      </script>
     </Helmet>
     <div style={{ display: `flex`, flexDirection: `column` }} >
-      <div style={{ display: `block` }} >
-        <div style={{ float: `left` }} >
+      <div style={{ display: `inline-flex` }} >
+        <div style={{ float: `left`, marginRight: `12px` }} >
           Pradeep Garigipati <a href="https://www.linkedin.com/in/pradeepgarigipati">(LinkedIn Profile)</a>
           <p>
             Freelance Software Engineer
@@ -29,8 +33,13 @@ export default function Home({data}) {
             GitHub username is <a href="https://github.com/9prady9">9prady9</a>
           </p>
         </div>
-        <div style={{ float: `right` }} >
+        <div style={{ float: `right`, marginRight: `12px`  }} >
           <img src={qrcode} style={{ maxWidth:128, width:`auto`, height:`auto` }} alt="qr code"/>
+        </div>
+        <div data-iframe-width="150"
+          data-iframe-height="270"
+          data-share-badge-id="a208c918-fed5-4c97-ae1f-d0bbd6e1052a"
+          data-share-badge-host="https://www.youracclaim.com">
         </div>
       </div>
       <Header headerLevel="2" text="Projects"/>
